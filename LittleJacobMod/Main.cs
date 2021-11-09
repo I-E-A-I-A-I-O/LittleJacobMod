182d680cf0415e5232882c78ed8534f53ec975a4
I-E-A-I-A-I-O <jesusdavid268@outlook.com>
2021-11-08 21:13:00 -0400

    Fixed using incorrect native to check the model

--- LittleJacobMod/Main.cs
+++ LittleJacobMod/Main.cs
@@ -129,7 +129,7 @@ public class Main : Script
 
     void ModelWatcher(object o, EventArgs e)
     {
-        if (Function.Call<bool>(Hash.IS_PED_IN_MODEL, Game.Player.Character.Handle, CurrentPed))
+        if (Function.Call<bool>(Hash.IS_PED_MODEL, Game.Player.Character.Handle, CurrentPed))
         {
             return;
         }
